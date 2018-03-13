43f3b01496d81d44fcfac02f16352985138f23c0
sathishAB <37295788+sathishAB@users.noreply.github.com>
2018-03-13 17:42:28 +0530

    vowelsconsonant

---
+++ vowelconsonant.py
@@ -0,0 +1,8 @@
+a = input("Input a letter of the alphabet: ")
+if a in ('a', 'e', 'i', 'o', 'u'):
+	print("%s is a vowel." % a)
+elif a == 'y':
+	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+else:
+	print("%s is a consonant." % a) 
+	
